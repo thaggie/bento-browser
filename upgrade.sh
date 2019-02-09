ed45ac1644ad4eb3b3171777bb3697757af290f6
Tom Haggie <thaggie@gmail.com>
2019-02-09 12:17:48 -0800

    Upgraded dependencies

---
+++ upgrade.sh
@@ -0,0 +1,2 @@
+yarn remove "electron" "electron-packager" "eslint-config-prettier" "eslint-plugin-prettier" "prettier" "electron-default-menu" "isomorphic-fetch"
+yarn add "electron" "electron-packager" "eslint-config-prettier" "eslint-plugin-prettier" "prettier" "electron-default-menu" "isomorphic-fetch"
